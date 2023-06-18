a9c3e4f358dce0be8675b2276c3aefa99cba317b
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-18 00:36:46 -0600

    Programa que suma dos numeros pedidos desde el teclado

---
+++ 00_Ejercicios_Introductorios/SumaDeNumeros/src/sumadenumeros/SumaDeNumeros.java
@@ -0,0 +1,16 @@
+package sumadenumeros;
+
+/**
+ *
+ * @author erick
+ */
+public class SumaDeNumeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
