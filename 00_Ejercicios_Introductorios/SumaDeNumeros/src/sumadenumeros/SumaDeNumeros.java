78765c260f18ad96314884768008d296f30a13de
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-18 00:48:53 -0600

    Programa que suma dos numeros finalizado

--- 00_Ejercicios_Introductorios/SumaDeNumeros/src/sumadenumeros/SumaDeNumeros.java
+++ 00_Ejercicios_Introductorios/SumaDeNumeros/src/sumadenumeros/SumaDeNumeros.java
@@ -1,16 +1,25 @@
 package sumadenumeros;
 
+import java.util.Scanner;
 /**
- *
  * @author erick
  */
 public class SumaDeNumeros {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+         int numero1, numero2;
+         Scanner sc = new Scanner(System.in);
+       
+        System.out.println("Por favor ingrese dos numeros enteros para poder calcular su suma");
+        
+        System.out.print("Por favor ingrese el primer numero: ");
+        numero1 = sc.nextInt();
+        
+        System.out.print("Por favor ingrese el segundo numero: ");
+        numero2 = sc.nextInt();
+        
+        System.out.println("La suma de los numeros ingresados es: " + (numero1 + numero2));
+        
     }
     
 }
